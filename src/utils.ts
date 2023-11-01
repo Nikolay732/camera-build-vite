eee0b7e1619e29c05033717f058fc2da18668cc0
Николай Бабаркин <nikola_@bk.ru>
2023-11-01 21:09:27 +0300

    fix: Изменил локаль даты отзывов на Ru

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs';
 import { Review } from './types/review';
+import 'dayjs/locale/ru';
 
-export const getFormatDate = (date: string, format: string) => dayjs(date).format(format);
+export const getFormatDate = (date: string, format: string) => dayjs(date).locale('ru').format(format);
 
 export const getSortByDate = (a: Review, b: Review) => {
   const dateA = new Date(a.createAt);
